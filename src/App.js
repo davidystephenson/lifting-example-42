06b206f2a8dbd64c6352817c00401f98e3500a5c
David Y. Stephenson <david@davidystephenson.com>
2020-07-02 22:29:04 +0200

    Lift state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Child ({ child, makeChildHungry }) {
+  // const [hungry, setHungry] = useState()
+  const { age, hungry } = child
+
+  const message = hungry ? 'I am hungry' : 'I am not hungry'
+
+  function onClick () {
+    makeChildHungry(age)
+    // setHungry(true)
+  }
+
+  return <p>
+    I am {age} years old, and {message}
+    {' '}
+    <button onClick={onClick}>Hungry</button>
+  </p>
 }
 
-export default App;
+function Parent () {
+  const initial = [
+    { age: 10, hungry: false },
+    { age: 12, hungry: false },
+    { age: 6, hungry: false }
+  ]
+  const [
+    childInformations, setChildInformations
+  ] = useState(initial)
+
+  function makeChildHungry (age) {
+    console.log('age test:', age)
+
+    const newChildInformations = childInformations
+      .map(childInformation => {
+        const match = age === childInformation.age
+
+        if (match) {
+          const newChildInformation = {
+            ...childInformation, hungry: true
+          }
+
+          return newChildInformation
+        }
+
+        return childInformation
+      })
+
+    setChildInformations(newChildInformations)
+  }
+
+  // iterate - do something for each one
+  const children = childInformations
+    .map(childInformation => (
+      <Child
+        child={childInformation}
+        makeChildHungry={makeChildHungry}
+      />
+    ))
+
+  // aggregate - do something for everything all together
+  const hungries = childInformations.filter(
+    childInformation => childInformation.hungry
+  )
+  const total = hungries.length
+
+  return <div>
+    {children}
+
+    We need to make {total} meals.
+  </div>
+}
+
+function App() {
+  return <div>
+    hello
+    <Parent />
+  </div>
+}
+
+export default App
